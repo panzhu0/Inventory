6c96604fda29f17c308615be33d51d03e7b6561d
panzhuo <zhuo_pan@icloud.com>
2025-03-24 18:46:17 +0800

    java

---
+++ inventory/src/test/java/com/github/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
